47d7e5215f4a9faf7f0a20bf51c99bca40a3b9ae
Tsawwassen <mitchell.rian.smith@gmail.com>
2020-05-05 08:50:04 -0700

    Add description to home page

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -10,7 +10,13 @@ class Home extends Component {
 
   render () {
     return (
-    	<h1>Home</h1>
+      <div>
+      	<h1>Welcome to the Ticket Tracker App</h1>
+        <p>This app allows you to create and track tickets</p>
+        <p>Tickets include stores, parts, and ticket status</p>
+        <p>You are able to add, view, and edit parts and stores at any time</p>
+        <p>Once you have added parts and stores, you can select them in the ticket screen</p>
+      </div>
     );
   }
 }
