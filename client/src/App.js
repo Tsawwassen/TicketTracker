fb5ae39c70bf474cd79a72a0242893f740f4f0d1
Tsawwassen <mitchell.rian.smith@gmail.com>
2020-05-05 08:46:20 -0700

    Organize imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import './App.css';
+
+//Component Imports
+import Home from './components/home/home.js';
 import Navigation from './components/navigation/navigation.js';
 import Stores from './components/stores/stores.js';
 
+
+//CSS Imports
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <Stores />
+      <Home />
     </div>
   );
 }
