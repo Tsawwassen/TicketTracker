9e09b8dc4f0a144a4c871e06c92ee999c961e991
Tsawwassen <mitchell.rian.smith@gmail.com>
2020-05-05 09:01:04 -0700

    Add navigation links

--- client/src/components/navigation/navigation.js
+++ client/src/components/navigation/navigation.js
@@ -15,7 +15,7 @@ class Navigation extends Component {
         <Navbar bg="dark" variant="dark">
           <Navbar.Brand href="/">T.T.A.</Navbar.Brand>
           <Nav className="mr-auto">
-            <Nav.Link href="#">Stores</Nav.Link>
+            <Nav.Link href="/stores">Stores</Nav.Link>
             <Nav.Link href="#">Parts</Nav.Link>
             <Nav.Link href="#">Tickets</Nav.Link>
           </Nav>
