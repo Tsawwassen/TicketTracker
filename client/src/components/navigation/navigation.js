b792b7c886e40936bbad4b73bcbccd96782b82f4
Tsawwassen <mitchell.rian.smith@gmail.com>
2020-05-05 08:37:08 -0700

    Add Navbar to React project

---
+++ client/src/components/navigation/navigation.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {Navbar, Nav} from 'react-bootstrap';
+
+
+
+class Navigation extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="/">T.T.A.</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#">Stores</Nav.Link>
+            <Nav.Link href="#">Parts</Nav.Link>
+            <Nav.Link href="#">Tickets</Nav.Link>
+          </Nav>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default Navigation;
