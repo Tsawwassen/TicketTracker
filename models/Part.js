d36c0bac019eb31f99123f3b4c3b71ff064982a9
Tsawwassen <mitchell.rian.smith@gmail.com>
2020-05-08 15:14:42 -0700

    Tried adding unique to SKU filed to see if it would be unique, but it didnt do what I expected

--- models/Part.js
+++ models/Part.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
 
 const Part = new mongoose.Schema({
-	sku: {type:String},
-	name: {type:String},
-	desc: {type:String}
+	sku: {type: String},
+	name: {type: String},
+	desc: {type: String}
 });
 
 module.exports = mongoose.model('Part', Part);
