1f651c5243fa6076497881ce3913fb7128a226a3
Tsawwassen <mitchell.rian.smith@gmail.com>
2020-05-12 11:13:33 -0700

    Changed column type from Type to type as it was preventing records from getting created with the correct fields

--- models/Ticket.js
+++ models/Ticket.js
@@ -1,15 +1,14 @@
 var mongoose = require('mongoose');
 
 const Ticket = new mongoose.Schema({
-	//sku: {type: String},
 	//ticketNumber: {Type: Number} //DEV NOTE - It would be nice to have this value increment when a ticket is created
-	storeID: {Type: Number},
-	desc: {Type: String},
-	createDate: {Type: Date},
-	editDate: {Type: Date},
-	closedDate: {Type: Date},
-	partsList: [{ sku: {Type: String}}],
-	Notes: [ {body: {Type:String}, date: {Type: Date}}]
+	storeNumber: {type: Number},
+	desc: {type: String},
+	createDate: {type: String},
+	editDate: {type: String},
+	closedDate: {type: String},
+	partsList: [{ sku: {type: String}}],
+	notes: [ {body: {type:String}, date: {type: Date}}]
 });
 
 module.exports = mongoose.model('Ticket', Ticket);
